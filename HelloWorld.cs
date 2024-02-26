ccc4ab149ad92a266f2c3b48cd517181292a7632
silikonech4 <161357010+silikonech4@users.noreply.github.com>
2024-02-26 19:47:47 +0300

    Add files via upload
    просто script

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       //вывод консоль приветствие миру 
+        print("hello, world ");
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
